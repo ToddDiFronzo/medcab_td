29f92b13a92309bc748d9314d02b8e168a7051ee
Todd DiFronzo <fonze@aol.com>
2020-06-23 19:36:16 -0700

    json fix

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from pred_request import *
 
 app = Flask(__name__)
@@ -8,10 +8,12 @@ app = Flask(__name__)
 def root():
     return {"message": "Hello World"}
 
-@app.route("/predictions/", methods=['POST'])
-def predict(pred_request: PredRequest):
-    rec_strains = pred_request.pred()
-    return {"Recommended Strains": rec_strains}
+def predict():
+    request_json = request.get_json()
+    user_input = request_json.get("user_input")
+    pred_request = PredRequest(user_input=user_input)
+    pred = pred_request.pred()
+    return jsonify({"Prediction": pred})
 
 if __name__ == "__main__":
     app.run()
